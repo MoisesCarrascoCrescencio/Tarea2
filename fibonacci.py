40b6d702935dc44e5e7184ddf3436954509865a0
MoisesCarrascoCrescencio <moisescarrascocrescencio0002@gmail.com>
2024-02-16 23:43:36 -0600

    Logica principal para fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,23 @@
 print("Fibonacci")
+
+def fibonacci(numero):
+
+    if (numero <= 0):
+        return ("Ha ingresado un número negativo")
+
+    elif (numero == 1):
+        return 0
+
+    elif (numero == 2):
+        return 1
+
+    else:
+        pass
+
+
+
+
+print("Ingresa un número")
+numero = input()
+numero = int(numero)
+print(fibonacci(numero))
