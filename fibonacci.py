d0e8f37321d3638fd4b665a886781baa0ce53cc1
MoisesCarrascoCrescencio <moisescarrascocrescencio0002@gmail.com>
2024-02-17 00:35:14 -0600

    Cambie el algoritmo secuencial por uno recursivo.

--- fibonacci.py
+++ fibonacci.py
@@ -1,23 +1,12 @@
-print("Fibonacci")
-
 def fibonacci(numero):
-
-    if (numero <= 0):
-        return ("Ha ingresado un número negativo")
-
-    elif (numero == 1):
+    if numero <= 0:
+        return "Ha ingresado un número negativo"
+    elif numero == 1:
         return 0
-
-    elif (numero == 2):
+    elif numero == 2:
         return 1
-
     else:
-        a = 0
-        b = 1
-
-        for _ in range(2, numero):
-            a, b = b, a + b
-        return b
+        return fibonacci(numero - 1) + fibonacci(numero - 2)
 
 print("Ingresa un número")
 numero = int(input())
